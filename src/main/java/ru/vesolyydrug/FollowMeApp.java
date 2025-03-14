143514cbbdafe0ce724ee99297388a265702e3af
vesolyydrug <VesolyyDrug@gmail.com>
2025-03-14 11:57:45 +0300

    Перемещение машинки

--- src/main/java/ru/vesolyydrug/FollowMeApp.java
+++ src/main/java/ru/vesolyydrug/FollowMeApp.java
@@ -2,12 +2,24 @@ package ru.vesolyydrug;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FollowMeApp {
 
     public static void main(String[] args) {
         SpringApplication.run(FollowMeApp.class, args);
+    }
+
+    @Bean
+    public Database database() {
+        return new Database();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
